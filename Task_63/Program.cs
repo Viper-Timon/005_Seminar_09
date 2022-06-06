20cc680408a11ce65ac42b13109bb3316d660008
viper-timon <viper-timon@mail.ru>
2022-06-06 20:22:55 +0300

    1st commit 63 done

---
+++ Task_63/Program.cs
@@ -0,0 +1,17 @@
+﻿// от 1 до N
+// 1 2 3 4 5 6
+
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+NaturalNumber(n);
+Console.ReadKey();
+
+void NaturalNumber(int num)
+{
+    if (num == 0) return;
+    NaturalNumber(num-1);
+    Console.Write(num + " ");
+}
+
+
