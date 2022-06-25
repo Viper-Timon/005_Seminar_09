b7ce602937a4dddc8aafd289ab69899a789feb67
viper-timon <viper-timon@mail.ru>
2022-06-25 12:44:22 +0300

    Tasks 64, 66 done

---
+++ Task_66_HW/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M");
+int numM = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numN = int.Parse(Console.ReadLine());
+
+
+int FindSumNaturalBtwn(int m, int n)
+{
+    if (m > n)
+    {
+        Console.WriteLine("Число M больше числа N");
+        return m;
+    }
+    else if (m < n)
+    {
+        m = m + FindSumNaturalBtwn(m + 1, n);
+        return m;
+    }
+    else return m;
+}
+
+Console.WriteLine($"Сумма натуральных чисел между M и N = {FindSumNaturalBtwn(numM, numN)}");
+
