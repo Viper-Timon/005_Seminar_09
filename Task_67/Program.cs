1c3936f209a3aabaff106b47662c7af6446418f5
viper-timon <viper-timon@mail.ru>
2022-06-25 11:06:27 +0300

    add HW directory

---
+++ Task_67/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 67: Напишите программу, 
+// которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+
+Console.WriteLine("Введите число N");
+int numN = int.Parse(Console.ReadLine());
+int summa = 0;
+
+void GetSum(int n, int sum)
+{
+    if (n == 0)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+    sum += n % 10;
+    n = n / 10;
+    GetSum(n, sum);
+}
+
+GetSum(numN, 0);
+
---
+++ Task_69/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите число A");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numB = int.Parse(Console.ReadLine());
+
+//_________через VOID___________________________
+
+// void GetNumPow(int a, int b, int comp)
+// {
+//    if (b == 0)
+//     {
+//         Console.WriteLine(comp);
+//         return;
+//     }
+//     comp *= a;
+
+//     GetNumPow(a, b - 1, comp);
+
+// }
+
+//GetNumPow(numA, numB, 1);
+
+
+
+
+int GetNumPow2(int a, int b)
+{
+    if (b == 1) return a;
+
+    else
+    {
+        a = a * GetNumPow2(a, b - 1);
+        return a;
+    }
+
+}
+Console.WriteLine(GetNumPow2(numA, numB));
