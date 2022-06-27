73c127a285d6fd61ca5edbc64932ddb6c916d85a
viper-timon <viper-timon@mail.ru>
2022-06-27 17:31:30 +0300

    68 Akkerman done

--- Task_68_HW/Program.cs
+++ Task_68_HW/Program.cs
@@ -2,6 +2,25 @@
 // рекурсии. Даны два неотрицательных числа m и n.
 // m = 3, n = 2 -> A(m,n) = 29
 
+// https://ru.wikipedia.org/wiki/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8F_%D0%90%D0%BA%D0%BA%D0%B5%D1%80%D0%BC%D0%B0%D0%BD%D0%B0
+// в самой заголовке "Определение" функция вся была описана
 
+
+Console.Write("Введите число m -> ");
+int numM = int.Parse(Console.ReadLine());
+Console.Write("Введите число n -> ");
+int numN = int.Parse(Console.ReadLine());
+
+
+int A(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return A(m - 1, 1);
+    if (m > 0 && n > 0) return A(m - 1, A(m, n - 1));
+    return A(m, n);
+}
+
+
+Console.WriteLine(A(numM, numN));
 
 
